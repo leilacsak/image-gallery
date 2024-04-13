45ce6b33aee3d30eefd798b3e4dc0fa2db88fb29
leilacsak <leilacsak@gmail.com>
2024-04-13 19:02:53 +0200

    fetching images endpoint

--- src/main/java/com/example/imagegallery/controllers/ImageController.java
+++ src/main/java/com/example/imagegallery/controllers/ImageController.java
@@ -1,10 +1,13 @@
 package com.example.imagegallery.controllers;
 
 import com.example.imagegallery.entities.Image;
+import com.example.imagegallery.services.GalleryService;
 import com.example.imagegallery.services.ImageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/images")
@@ -13,27 +16,35 @@ public class ImageController {
 
     private final ImageService imageService;
 
+
+
     @Autowired
     public ImageController(ImageService imageService){
         this.imageService = imageService;
     }
 
+    @GetMapping("/gallery/{GalleryId}")
+    public ResponseEntity<List<Image>> getImagesByGalleryId(@PathVariable Long GalleryId) {
+        List<Image> images = imageService.getImagesByGalleryId(GalleryId);
+        return ResponseEntity.ok(images);
+    }
+
     @PostMapping
     public ResponseEntity<Image> addImage(@RequestBody Image image) {
-        Image addedImage = imageService.addImage(image.getGallery().getGalleryid(),
+        Image addedImage = imageService.addImage(image.getGallery().getGalleryId(),
                 image.getFilename(), image.getDescription(), image.getImageData());
         return ResponseEntity.ok(addedImage);
     }
 
     @PutMapping("/{imageId}")
-    public ResponseEntity<Image> updateImage(@PathVariable Long imageId, @RequestBody Image updatedImage) {
-        Image image = imageService.updateImage(imageId, updatedImage.getDescription(), updatedImage.getFilename());
+    public ResponseEntity<Image> updateImage(@PathVariable Long ImageId, @RequestBody Image updatedImage) {
+        Image image = imageService.updateImage(ImageId, updatedImage.getDescription(), updatedImage.getFilename());
         return ResponseEntity.ok(image);
     }
 
     @DeleteMapping("/{imageId}")
-    public ResponseEntity<Void> deleteImage(@PathVariable Long imageid) {
-        imageService.deleteImage(imageid);
+    public ResponseEntity<Void> deleteImage(@PathVariable Long ImageId) {
+        imageService.deleteImage(ImageId);
         return ResponseEntity.noContent().build();
     }
 }
